ac11fdbd4c191ea6b285fd6dea80a29d374233b8
Nick Glenn <nick@helpfulhuman.com>
2015-11-22 17:12:34 -0800

    Fixing some export issues on index due to ES2015 updates

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import {factory as createServer} from './factories/server';
 import {factory as createFixture} from './factories/fixture';
 import {factory as createResource} from './factories/resource';
 
-export default {
+export {
 
-  create: createServer,
+  createServer as create,
 
-  fixture: createFixture,
+  createFixture as fixture,
 
-  resource: createResource,
+  createResource as resource
 
 }
--- test/index.js
+++ test/index.js
@@ -4,9 +4,9 @@ import restfool from '../src/index';
 describe('restfool', function () {
 
   it('contains the various factory functions', function () {
+    expect(restfool).to.have.property('create').that.is.a('function');
     expect(restfool).to.have.property('resource').that.is.a('function');
     expect(restfool).to.have.property('fixture').that.is.a('function');
-    // expect(restfool).to.have.property('').that.is.a('function');
   });
 
 
